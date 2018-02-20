1b56ced4f66c1b32fc7d7db6e41f351ff4c35631
Mitsui Yasuyuki <mitsui.y.k@gmail.com>
2018-02-21 03:01:53 +0900

    world charts layout

--- client/src/components/Step4.vue
+++ client/src/components/Step4.vue
@@ -7,44 +7,7 @@
           質問４：興味のある地域を教えてください
         </h2>
       </div>
-      <div class="select-btn">
-        <el-row :gutter="30" style="display: none">
-          <el-col :span="6">
-            <el-button @click="select('1')" :class="{'is-active':value.indexOf('1') >= 0}">欧州</el-button>
-          </el-col>
-          <el-col :span="6">
-            <el-button @click="select('2')" :class="{'is-active':value.indexOf('2') >= 0}">中近東</el-button>
-          </el-col>
-          <el-col :span="6">
-            <el-button @click="select('3')" :class="{'is-active':value.indexOf('3') >= 0}">日本</el-button>
-          </el-col>
-          <el-col :span="6">
-            <el-button @click="select('4')" :class="{'is-active':value.indexOf('4') >= 0}">北米</el-button>
-          </el-col>
-          <el-col :span="6">
-            <el-button @click="select('5')" :class="{'is-active':value.indexOf('5') >= 0}">アジア</el-button>
-          </el-col>
-          <el-col :span="6">
-            <el-button @click="select('6')" :class="{'is-active':value.indexOf('6') >= 0}">アフリカ</el-button>
-          </el-col>
-          <el-col :span="6">
-            <el-button @click="select('7')" :class="{'is-active':value.indexOf('7') >= 0}">オセアニア</el-button>
-          </el-col>
-          <el-col :span="6">
-            <el-button @click="select('8')" :class="{'is-active':value.indexOf('8') >= 0}">中南米</el-button>
-          </el-col>
-          <el-col :span="6">
-            <el-button @click="select('9')" :class="{'is-active':value.indexOf('9') >= 0}">グローバル</el-button>
-          </el-col>
-          <el-col :span="6">
-            <el-button @click="select('10')" :class="{'is-active':value.indexOf('10') >= 0}">エマージング</el-button>
-          </el-col>
-          <el-col :span="6">
-            <el-button @click="select('11')" :class="{'is-active':value.indexOf('11') >= 0}">おまかせ</el-button>
-          </el-col>
-        </el-row>
-      </div>
-      <div id="map" style="position:relative; width: 943px; height: 330px;"></div>
+      <div id="map"></div>
     </div>
     <div class="action-btn">
       <el-button-group>
@@ -61,14 +24,14 @@ import * as d3 from 'd3'
 import Datamap from 'datamaps'
 import _ from 'underscore'
 
-const BTN_CLASS_UNSELECTED = "btn btn-default";
-const BTN_CLASS_SELECTED = "btn btn-defalut active";
+const BTN_CLASS_UNSELECTED = "el-button el-button--default";
+const BTN_CLASS_SELECTED = "el-button el-button--default is-active";
 const AREA_LABELS = (function(){
   var result = [
     {id: 'JAPAN',              lat: 42,  lng: 149,  color: "#e377c2", countries: ["JPN"],                                     name: '日本'},
     {id: 'CHINA',              lat: 40,  lng: 100,  color: "#d62728", countries: ["CHN"],                                     name: '中国'},
     {id: 'INDIA',              lat: 20,  lng: 70,   color: "#2ca02c", countries: ["IND"],                                     name: 'インド'},
-    {id: 'ASIA',               lat: 10,  lng: 110,  color: "#ff7f0e", countries: _.difference(investAreas["NA"], ["JPN"]),    name: 'アジア'},
+    {id: 'ASIA',               lat: 10,  lng: 110,  color: "#ff7f0e", countries: _.difference(investAreas["AS"], ["JPN"]),    name: 'アジア'},
     {id: 'NORTH_AMERIA',       lat: 50,  lng: -110, color: "#9467db", countries: investAreas["NA"],                           name: '北米'},
     {id: 'SOUTH_AMERIA',       lat: -10, lng: -70,  color: "#8c564b", countries: investAreas["SA"],                           name: '南米'},
     {id: 'EUROPE',             lat: 60,  lng: 15,   color: "#68b0dd", countries: investAreas["EU"],                           name: '欧州'},
@@ -90,6 +53,7 @@ const AREA_LABELS = (function(){
       area.updateObject[country] = area.color;
     });
   });
+  return result;
 })();
 const COUNTRY_TO_AREA_MAP = (function() {
   var result = {};
@@ -102,7 +66,7 @@ const COUNTRY_TO_AREA_MAP = (function() {
     });
   });
   return result;
-});
+})();
 export default {
   name: 'Step4',
   data () {
@@ -128,8 +92,11 @@ export default {
       this.$router.push({ name: 'Step5' });
     }
   },
+  destroyed: function() {
+    d3.select(this.map.options.element).selectAll('*').remove();
+  },
   mounted: function() {
-    var map = new Datamap({
+    var map = this.map = new Datamap({
       element: document.getElementById('map'),
       fills: {
         // リセット時の色を設定
@@ -146,55 +113,60 @@ export default {
       },
       setProjection: function() {
         // メルカトル図法、日本中心とする
-        var projection = d3.geo.mercator()
+        var projection = d3.geoMercator()
           .center([0, -20])
           .scale(130)
           .rotate([-155, 0, 0]);
-        var path = d3.geo.path().projection(projection);
+        var path = d3.geoPath().projection(projection);
         return {path, projection}
       },
       data: COUNTRY_TO_AREA_MAP
     });
+    if (map.customAddLabel == null) {
+      map.addPlugin("customAddLabel", function(layer) {
+        var data = AREA_LABELS;
+        var _this = this;
+        d3.select(map.options.element)
+          .append("div")
+          .attr("class", "invest-area-map-label")
+          .selectAll(".invest-area-map-label")
+          .data(data, JSON.stringify)
+          .enter()
+          .append("button")
+          .attr("class", BTN_CLASS_UNSELECTED)
+          .style("left", function(d) {
+            if (d.x != null) {
+              return d.x + "px"
+            }
+            // 経緯度からピクセル座標に変換
+            var xy = _this.latLngToXY(d.lat, d.lng);
+            return xy[0] + "px";
+          })
+          .style("top", function(d) {
+            if (d.y != null) {
+              return d.y + "px"
+            }
+            // 経緯度からピクセル座標に変換
+            var xy = _this.latLngToXY(d.lat, d.lng);
+            return xy[1] + "px"
+          })
+          .html(function(d){return d.name;})
+          .on("mouseover", function(d) {return selectArea(d);})
+          .on("mouseout", function() {return clearSelectionAll();})
+          .on("click", function(d) {return notifyClickArea(d);});
+      });
+    }
 
-    map.addPlugin("customAddLabel", function(layer) {
-      var data = AREA_LABELS;
-      var _this = this;
-      d3.select(map.options.element)
-        .append("div")
-        .attr("class", "invest-area-map-label")
-        .selectAll(".invest-area-map-label")
-        .data(data, JSON.stringify)
-        .enter()
-        .append("button")
-        .attr("class", BTN_CLASS_UNSELECTED)
-        .style("left", function(d) {
-          if (d.x != null) {
-            return d.x + "px"
-          }
-          // 経緯度からピクセル座標に変換
-          var xy = _this.latLngToXY(d.lat, d.lng);
-          return xy[0] + "px";
-        })
-        .style("top", function(d) {
-          if (d.y != null) {
-            return d.y + "px"
-          }
-          // 経緯度からピクセル座標に変換
-          var xy = _this.latLngToXY(d.lat, d.lng);
-          return xy[0] + "px"
-        })
-        .html(function(d){return d.name;})
-        .on("mouseover", function(d) {return selectArea(d);})
+    if(map.customHandleMouseEvent == null ) {
+      map.addPlugin("customHandleMouseEvent", function(layer) {
+        this.svg.selectAll(".datamaps-subunit")
+        .on("mouseover", function(d) {return selectAreaByCountry(d.id);})
         .on("mouseout", function() {return clearSelectionAll();})
-        .on("click", function(d) {return notifyClickArea(d);});
-    });
-    map.addPlugin("customHandleMouseEvent", function(layer) {
-      this.svg.selectAll(".datamaps-subunit")
-      .on("mouseover", function(d) {return selectAreaByCountry(d.id);})
-      .on("mouseout", function() {return clearSelectionAll();})
-      .on("click", function(d) {return notifyClickArea(COUNTRY_TO_AREA_MAP[d.id]);});
-    });
+        .on("click", function(d) {return notifyClickArea(COUNTRY_TO_AREA_MAP[d.id]);});
+      });
+    }
 
+    var clickedAreas = [];
     var selectedArea = null;
     const clearSelectionAll = function() {
       selectedArea = null;
@@ -217,7 +189,7 @@ export default {
         .selectAll(".invest-area-map-label")
         .selectAll("button")
         .attr("class", function(item) {
-          if (area && item.id === area.id) {
+          if ((area && item.id === area.id) || clickedAreas.indexOf(item) >= 0) {
             return BTN_CLASS_SELECTED
           } else {
             return BTN_CLASS_UNSELECTED
@@ -225,7 +197,12 @@ export default {
         });
     };
     const notifyClickArea = function(area) {
-      console.log('click', area);
+      let index = clickedAreas.indexOf(area);
+      if (index >= 0) {
+        clickedAreas.splice(index, 1);
+      } else {
+        clickedAreas.push(area);
+      }
     };
     map.customAddLabel();
     map.customHandleMouseEvent();
@@ -233,10 +210,15 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 #map {
-  button {
+  position: relative;
+  width: 943px;
+  height: 330px;
+  margin-left: auto;
+  margin-right: auto;
+
+  /deep/ .el-button {
     position: absolute;
   }
 }
